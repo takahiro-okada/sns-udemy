698e6ee80f1d0e405e7da2eb82d2476a1f706471
takahiro-okada <okahanako09875@gmail.com>
2024-04-05 05:39:14 +0900

    update: add cors package

--- api/server.js
+++ api/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const authRoute = require("./routers/auth");
+const cors = require("cors");
 
 require("dotenv").config();
 
 const PORT = 5001;
 
-
+app.use(cors());
 app.use(express.json());
 
 // ブラウザで5001ポートにアクセスすると、Hello Worldが表示される
