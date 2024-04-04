cd9456a96f93261f2f9450dae6e4813f95f314c3
takahiro-okada <okahanako09875@gmail.com>
2024-04-05 05:05:59 +0900

    update: add NabBar

---
+++ client/components/NavBar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+
+const NavBar = () => {
+  return (
+    <header className="bg-gray-700 p-4 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="font-semibold text-xl">
+          <Link href="/">SNS Clone</Link>
+        </h1>
+        <nav>
+          <ul className="flex space-x-4">
+            <>
+              <Link
+                href="/login"
+                className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
+              >
+                ログイン
+              </Link>
+              <Link
+                href="/signup"
+                className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
+              >
+                サインアップ
+              </Link>
+            </>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
