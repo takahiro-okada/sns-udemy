6c72f14f04b5ec97b8c535ced3b41796ed16311b
takahiro-okada <okahanako09875@gmail.com>
2024-04-06 07:54:55 +0900

    update: add latest posts view

--- client/components/Timeline.tsx
+++ client/components/Timeline.tsx
@@ -1,16 +1,54 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 import Post from "./Post";
+import apiClient from "@/lib/apiClients";
+import { PostType } from "@/types";
 
 const Timeline = () => {
+  const [postText, setPostText] = useState<string>("");
+  const [latestPosts, setLatestPosts] = useState<PostType[]>([]);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const newPost = await apiClient.post("/posts/post", {
+        content: postText,
+      });
+      setLatestPosts((prevPosts) => [newPost.data, ...prevPosts]);
+      setPostText("");
+    } catch (err) {
+      alert("ログインしてください");
+    }
+  };
+
+  useEffect(() => {
+    const fetchLatestPosts = async () => {
+      try {
+        const response = await apiClient.get("/posts/get_latest_post");
+        setLatestPosts(response.data);
+      } catch(error) {
+        console.log(error);
+      }
+    };
+
+    fetchLatestPosts();
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-100">
       <main className="container mx-auto py-4">
         <div className="bg-white shadow-md rounded p-4 mb-4">
-          <form>
+          <form onSubmit={handleSubmit}>
             <textarea
               className="w-full h-24 p-2 border border-gray-300 rounded resize-none focus:outline-none focus:ring-2 focus:ring-blue-400"
               placeholder="What's on your mind?"
+              onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                setPostText(e.target.value)
+              }
+              value={postText}
             ></textarea>
             <button
               type="submit"
@@ -20,13 +58,9 @@ const Timeline = () => {
             </button>
           </form>
         </div>
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
+        {latestPosts.map((post: PostType) => (
+          <Post key={post.id} post={post} />
+        ))}
       </main>
     </div>
   );
